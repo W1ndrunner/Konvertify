a59d15ad8278ce2b5974e91912ced3bc228c4375
lewis <lewislanek3264@gmail.com>
2026-06-01 21:54:24 +0100

    AWS client setup, DB connection set up, POST endpoint completed

---
+++ backend/Clients/AwsClient.cs
@@ -0,0 +1,42 @@
+﻿using Amazon.S3.Model;
+using Amazon;
+using System;
+using System.Linq.Expressions;
+using Amazon.S3;
+
+namespace backend.Clients;
+
+public class AwsClient
+{
+    private readonly string bucketName = "uploads-891377227248-eu-north-1-an";
+    private readonly int urlDuration = 1; // Hours
+    private IAmazonS3 _s3Client = new AmazonS3Client(RegionEndpoint.EUNorth1);
+    
+    internal string CreatePresignedUrl(string filename)
+    {
+        string urlString = "";
+        try
+        {
+            var request = new GetPreSignedUrlRequest()
+            {
+                BucketName = bucketName,
+                Key = filename,
+                Expires = DateTime.UtcNow.AddHours(urlDuration)
+            };
+            urlString = _s3Client.GetPreSignedURL(request);
+
+        }
+        catch (AmazonS3Exception ex)
+        {
+            Console.WriteLine($@"Error: {ex.Message}");
+        }
+
+        if (string.IsNullOrEmpty(urlString))
+        {
+            throw new ArgumentNullException();
+        }
+
+        return urlString;
+    }
+
+}
--- backend/Program.cs
+++ backend/Program.cs
@@ -1,11 +1,23 @@
 using System;
+using System.Data;
+using System.Runtime.InteropServices.JavaScript;
 using NanoidDotNet;
+using backend.Clients;
+using Dapper;
+using Npgsql;
 
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+if (string.IsNullOrEmpty(connectionString))
+{
+    throw new ArgumentNullException();
+}
 
+builder.Services.AddNpgsqlDataSource(connectionString);
 builder.Services.AddOpenApi();
-
+builder.Services.AddSingleton<AwsClient>();
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -15,18 +27,31 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.MapPost("api/jobs", () =>
+app.MapPost("api/jobs", async (NpgsqlDataSource dataSource, CreateJobRequest request, AwsClient awsClient) =>
     {
         string jobUuid = Guid.NewGuid().ToString();
         string webhookToken = Nanoid.Generate();
-        string s3Url = GenerateS3Url();
-        
 
+        await using var connection = await dataSource.OpenConnectionAsync();
+
+        var result = await connection.ExecuteAsync(
+            "INSERT INTO jobs (id, status, webhook_token, s3_key) VALUES (@Id,'Pending', @WebhookToken, @Filename)",
+            new
+            {
+                Id = jobUuid,
+                Filename = request.Filename,
+                WebhookToken = webhookToken
+            });
+        string s3Url = awsClient.CreatePresignedUrl(request.Filename);
+
+        return Results.Ok(new
+        {
+            JobId = jobUuid,
+            UploadUrl = s3Url
+        });
     })
     .WithName("CreateJob");
 
-string GenerateS3Url()
-{
-    return "";
-}
 app.Run();
+
+public record CreateJobRequest(string Filename);
